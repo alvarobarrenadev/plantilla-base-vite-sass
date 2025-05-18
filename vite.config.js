6e0a3fa36cf715c9c7ef6a558e0aa8f4c5383f1f
Álvaro Barrena <alvarobarrena.dev@gmail.com>
2025-05-18 17:49:02 +0200

    Plantilla base para proyectos con Sass + Vite usando la carpeta src.

---
+++ vite.config.js
@@ -0,0 +1,39 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      // Alias principal para la carpeta src
+      '@': resolve(__dirname, './src'),
+      
+      // Alias específicos para Sass
+      '@sass': resolve(__dirname, './src/sass'),
+      '@abstracts': resolve(__dirname, './src/sass/abstracts'),
+      '@components': resolve(__dirname, './src/sass/components'),
+      '@layout': resolve(__dirname, './src/sass/layout'),
+      '@themes': resolve(__dirname, './src/sass/themes'),
+      '@base': resolve(__dirname, './src/sass/base'),
+      '@pages': resolve(__dirname, './src/sass/pages'),
+      '@vendors': resolve(__dirname, './src/sass/vendors')
+    }
+  },
+  css: {
+    devSourcemap: true, // Facilita la depuración del Sass
+    preprocessorOptions: {
+      scss: {
+        // Usa includePaths en lugar de loadPaths
+        includePaths: [
+          resolve(__dirname, './src/sass'),
+          resolve(__dirname, './src/sass/abstracts'),
+          resolve(__dirname, './src/sass/components'),
+          resolve(__dirname, './src/sass/layout'),
+          resolve(__dirname, './src/sass/base'),
+          resolve(__dirname, './src/sass/pages'),
+          resolve(__dirname, './src/sass/themes'),
+          resolve(__dirname, './src/sass/vendors'),
+        ]
+      }
+    }
+  }
+});
