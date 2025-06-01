1b7f24e676946f25522a9df3046978cbcb47626c
Álvaro Barrena <alvarobarrena.dev@gmail.com>
2025-06-01 16:57:12 +0200

    Añadir ruta base del nombre del repositorio para su posterior depliegue

--- vite.config.js
+++ vite.config.js
@@ -2,37 +2,38 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  base: "/NOMBRE_DEL_REPO/",
   resolve: {
     alias: {
       // Alias principal para la carpeta src
-      '@': resolve(__dirname, './src'),
-      
+      "@": resolve(__dirname, "./src"),
+
       // Alias específicos para Sass
-      '@sass': resolve(__dirname, './src/sass'),
-      '@abstracts': resolve(__dirname, './src/sass/abstracts'),
-      '@components': resolve(__dirname, './src/sass/components'),
-      '@layout': resolve(__dirname, './src/sass/layout'),
-      '@themes': resolve(__dirname, './src/sass/themes'),
-      '@base': resolve(__dirname, './src/sass/base'),
-      '@pages': resolve(__dirname, './src/sass/pages'),
-      '@vendors': resolve(__dirname, './src/sass/vendors')
-    }
+      "@sass": resolve(__dirname, "./src/sass"),
+      "@abstracts": resolve(__dirname, "./src/sass/abstracts"),
+      "@components": resolve(__dirname, "./src/sass/components"),
+      "@layout": resolve(__dirname, "./src/sass/layout"),
+      "@themes": resolve(__dirname, "./src/sass/themes"),
+      "@base": resolve(__dirname, "./src/sass/base"),
+      "@pages": resolve(__dirname, "./src/sass/pages"),
+      "@vendors": resolve(__dirname, "./src/sass/vendors"),
+    },
   },
   css: {
     devSourcemap: true, // Facilita la depuración de Sass
     preprocessorOptions: {
       scss: {
         includePaths: [
-          resolve(__dirname, './src/sass'),
-          resolve(__dirname, './src/sass/abstracts'),
-          resolve(__dirname, './src/sass/components'),
-          resolve(__dirname, './src/sass/layout'),
-          resolve(__dirname, './src/sass/base'),
-          resolve(__dirname, './src/sass/pages'),
-          resolve(__dirname, './src/sass/themes'),
-          resolve(__dirname, './src/sass/vendors'),
-        ]
-      }
-    }
-  }
+          resolve(__dirname, "./src/sass"),
+          resolve(__dirname, "./src/sass/abstracts"),
+          resolve(__dirname, "./src/sass/components"),
+          resolve(__dirname, "./src/sass/layout"),
+          resolve(__dirname, "./src/sass/base"),
+          resolve(__dirname, "./src/sass/pages"),
+          resolve(__dirname, "./src/sass/themes"),
+          resolve(__dirname, "./src/sass/vendors"),
+        ],
+      },
+    },
+  },
 });
